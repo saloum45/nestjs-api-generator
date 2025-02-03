dbe5ef46479c200815f5ea0c6cfe478b9fb20021
saloum45 <saloumfall45@gmail.com>
2025-02-03 08:46:49 +0000

    in progress

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ import { getTables, generateResources } from "./db";
 
 // Configuration de la base de données
 const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "nestapp",
+    type: process.env.DB_TYPE as any, // Cast nécessaire pour TypeORM
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME,
+      synchronize: false,
 });
 
 
