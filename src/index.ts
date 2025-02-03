665e04e79282d1a0ebcd55317d0c8bd7db460788
saloum45 <saloumfall45@gmail.com>
2025-02-03 08:50:22 +0000

    in progress

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { getTables, generateResources } from "./db";
 
 
 // Configuration de la base de données
 const AppDataSource = new DataSource({
     type: process.env.DB_TYPE as any, // Cast nécessaire pour TypeORM
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '3306', 10),
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME,
-      synchronize: false,
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '3306', 10),
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME,
+    synchronize: false,
 });
 
 
